ba29fbabfa80a592793f9e3af18dc33503a2c3f7
Tapoban Ray <raytapoban@gmail.com>
2025-12-21 22:00:31 +0530

    Built the ItemChatSheet UI

---
+++ ai_personal_content_app/lib/core/theme/app_svgs.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+SvgPicture robotFaceSvg(){
+  return SvgPicture.asset("assets/images/svgs/robot.svg");
+}
--- ai_personal_content_app/lib/features/items/screens/view_item_screen.dart
+++ ai_personal_content_app/lib/features/items/screens/view_item_screen.dart
@@ -1,12 +1,27 @@
 import 'package:ai_personal_content_app/core/theme/app_colors.dart';
+import 'package:ai_personal_content_app/core/theme/app_svgs.dart';
 import 'package:ai_personal_content_app/core/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 
-class ViewItemScreen extends StatelessWidget {
+class ViewItemScreen extends StatefulWidget {
   const ViewItemScreen({super.key});
+
+  @override
+  State<ViewItemScreen> createState() => _ViewItemScreenState();
+}
+
+class _ViewItemScreenState extends State<ViewItemScreen> {
+  late final GlobalKey<ScaffoldState> _scaffoldKey;
+
+  @override
+  void initState() {
+    _scaffoldKey = GlobalKey();
+    super.initState();
+  }
+
   // void _showFoodSearchBottomSheet(BuildContext parentContext) {
   //
   //   _scaffoldKey.currentState?.showBottomSheet(enableDrag: false, (
@@ -37,9 +52,16 @@ class ViewItemScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final optionsData = [
-      (icon: Icons.chat_outlined, color: null, label: "Chat", onTap: () {
-        
-      }),
+      (
+        icon: Icons.chat_outlined,
+        color: null,
+        label: "Chat",
+        onTap: () {
+          _scaffoldKey.currentState?.showBottomSheet(
+            (context) => _ChatBottomSheetContent(scaffoldKey: _scaffoldKey),
+          );
+        },
+      ),
       (icon: Icons.manage_search, color: null, label: "Search", onTap: () {}),
       (icon: Icons.push_pin_outlined, color: null, label: "Pin", onTap: () {}),
       (
@@ -57,6 +79,7 @@ class ViewItemScreen extends StatelessWidget {
     ];
 
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         leading: GestureDetector(
           onTap: () {
@@ -207,6 +230,269 @@ class _ItemOptionsButton extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _AiResponseMessageBox extends StatelessWidget {
+  const _AiResponseMessageBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 12.w,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        CircleAvatar(
+          backgroundColor: AppColors.blueGreyColor,
+          child: robotFaceSvg(),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 4.h,
+            children: [
+              Text(
+                "AI Assistant",
+                style: TextStyle(
+                  fontSize: 13.sp,
+                  color: AppColors.inactiveColor,
+                  fontVariations: [FontVariation.weight(400)],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.w),
+                decoration: BoxDecoration(
+                  color: AppColors.blueGreyColor,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(8.r),
+                  ).copyWith(bottomRight: Radius.circular(8.r)),
+                ),
+                child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere enim arcu, sed vehicula enim iaculis sit amet. Donec mattis nibh vel maximus tincidunt. Nunc feugiat quam est. Proin tincidunt velit eget libero.",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontVariations: [FontVariation.weight(500)],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HumanMessageBox extends StatelessWidget {
+  const _HumanMessageBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 12.w,
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            spacing: 4.h,
+            children: [
+              Text(
+                "Me",
+                style: TextStyle(
+                  fontSize: 13.sp,
+                  color: AppColors.inactiveColor,
+                  fontVariations: [FontVariation.weight(400)],
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.w),
+                decoration: BoxDecoration(
+                  color: AppColors.blueColor,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(8.r),
+                  ).copyWith(bottomLeft: Radius.circular(8.r)),
+                ),
+                child: Text(
+                  "Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, ",
+                  style: TextStyle(
+                    fontSize: 16.sp,
+                    fontVariations: [FontVariation.weight(500)],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        CircleAvatar(
+          backgroundColor: AppColors.blueGreyColor,
+          child: Text(
+            "I",
+            style: TextStyle(
+              fontSize: 20.sp,
+              fontVariations: [FontVariation.weight(600)],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ChatBottomSheetContent extends StatefulWidget {
+  final GlobalKey<ScaffoldState> scaffoldKey;
+
+  const _ChatBottomSheetContent({super.key, required this.scaffoldKey});
+
+  @override
+  State<_ChatBottomSheetContent> createState() =>
+      _ChatBottomSheetContentState();
+}
+
+class _ChatBottomSheetContentState extends State<_ChatBottomSheetContent> {
+  final FocusNode _focusNode = FocusNode();
+  final TextEditingController _userMsgController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return TapRegion(
+      onTapOutside: (event) {
+        widget.scaffoldKey.currentContext?.pop();
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 300),
+        curve: Curves.easeOut,
+        height: getScreenHeight(context) * 0.85,
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.w),
+        decoration: BoxDecoration(
+          color: AppColors.backgroundColor,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: 40.w,
+              height: 4.w,
+              decoration: BoxDecoration(color: AppColors.lightBlueGreyColor),
+            ),
+            14.verticalSpace,
+            SizedBox(
+              width: 335.w,
+              child: Text(
+                "Chats are not saved. All messages are deleted when you leave.",
+                style: TextStyle(
+                  fontSize: 14.sp,
+                  fontVariations: [FontVariation.weight(500)],
+                  color: AppColors.inactiveColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Divider(),
+            Expanded(
+              child: ListView.separated(
+                itemCount: 50,
+                separatorBuilder: (context, index) => SizedBox(height: 16.h),
+                reverse: true,
+                itemBuilder: (context, index) => _AiResponseMessageBox(),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              spacing: 8.h,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.blueGreyColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                  ),
+                  child: Row(
+                    spacing: 8.w,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(Icons.note_add_outlined, color: Colors.white),
+                      Text(
+                        "Save as Note",
+                        style: TextStyle(
+                          fontSize: 14.sp,
+                          color: Colors.white,
+                          fontVariations: [FontVariation.weight(600)],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  spacing: 12.w,
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        controller: _userMsgController,
+                        focusNode: _focusNode,
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.r),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.r),
+                            borderSide: BorderSide.none,
+                          ),
+                          focusedErrorBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.r),
+                            borderSide: BorderSide.none,
+                          ),
+                          filled: true,
+                          fillColor: AppColors.blueGreyColor,
+                          hintText: "Ask anything...",
+                          hintStyle: TextStyle(
+                            fontSize: 16.sp,
+                            color: AppColors.inactiveColor,
+                            fontVariations: [FontVariation.weight(500)],
+                          ),
+                        ),
+                        cursorColor: Colors.white,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                          fontVariations: [FontVariation.weight(500)],
+                        ),
+                        keyboardType: TextInputType.text,
+                        onTapOutside: (event) {
+                          _focusNode.unfocus();
+                        },
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColors.blueColor,
+                        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                        minimumSize: Size.square(50.w),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.r),
+                        ),
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.zero,
+                      ),
+                      child: Icon(
+                        Icons.arrow_upward_sharp,
+                        color: Colors.white,
+                        size: 25.w,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
